d113b3e25fae3e38aba2108eff327a8dd88efe81
poroolyosh <poroolyosh@gmail.com>
2022-09-13 21:13:12 +0300

    video-click

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function () {
     loop: true,
   });
 });
+
+/* Video */
+const videoBtn = document.querySelector('#video-btn');
+const videoPicture = document.querySelector('video__picture');
+
+videoBtn.addEventListener('click', function () {
+  videoPicture.classList.add('none');
+});
