9b5fb4ed85140251ee6419bcf1d7ea940ce59d7d
poroolyosh <poroolyosh@gmail.com>
2022-09-13 22:10:46 +0300

    2022-09-13

--- js/main.js
+++ js/main.js
@@ -8,8 +8,19 @@ $(document).ready(function () {
 
 /* Video */
 const videoBtn = document.querySelector('#video-btn');
-const videoPicture = document.querySelector('video__picture');
+const videoPicture = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
 
-videoBtn.addEventListener('click', function () {
-  videoPicture.classList.add('none');
+videoWrapper.addEventListener('click', function () {
+  if (video.paused) {
+    videoPicture.classList.add('none');
+    videoWrapper.classList.remove('video-overlay');
+    videoBtn.classList.add('none');
+    video.play();
+  } else {
+    videoWrapper.classList.add('video-overlay');
+    videoBtn.classList.remove('none');
+    video.pause();
+  }
 });
